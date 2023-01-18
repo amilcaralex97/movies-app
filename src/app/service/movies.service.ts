7db9419f2e2077225c7ec42ed84223d03a115b81
Amílcar Sánchez <45496427+amilcaralex97@users.noreply.github.com>
2023-01-17 23:33:01 -0600

    home and slider wip

---
+++ src/app/service/movies.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoviesService {
+  constructor(private http: HttpClient) {}
+
+  getMovies() {
+    return this.http.get('https://api.themoviedb.org/3/movie/upcoming?api_key=784a2b700b30245a8ba6bc06aa7883e8');
+  }
+}
